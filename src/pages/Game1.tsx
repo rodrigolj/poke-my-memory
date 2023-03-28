18e5d94f286e8cf0869ac087b9b89f4176f054b8
Rodrigo Lima Jaroszewski <rlima@orulo.com.br>
2023-03-28 02:36:27 -0300

    Começando a fazer a lógica de jogo mais elaborada

--- src/pages/Game1.tsx
+++ src/pages/Game1.tsx
@@ -1,6 +1,6 @@
-import { useEffect, useState } from "react";
-import styled from "styled-components";
-import TextInput from "../components/TextInput";
+import { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import TextInput from '../components/TextInput';
 
 const Container = styled.div`
     display: grid;
@@ -9,7 +9,7 @@ const Container = styled.div`
     align-items: start;
     width: 22rem;
     padding: 1rem;
-`
+`;
 
 const Section = styled.div`
     display: flex;
@@ -20,86 +20,102 @@ const Section = styled.div`
     padding: 1rem;
     min-height: 4rem;
     overflow: hidden;
-`
+`;
 
 const Title = styled.h1`
     font-size: 1.5rem;
     line-height: 2rem;
     font-weight: 100;
-    color: #0075BE;
-`
-
-const NameBox = styled.span`
-    padding: 2rem;
-`
+    color: #0075be;
+`;
 
 const PokemonImage = styled.img`
     max-height: 8rem;
     max-width: 8rem;
-`
+`;
 const Warning = styled.p`
     font-size: 0.75em;
-    color: #0A285F;
+    color: #0a285f;
     text-align: center;
-`
+`;
 
 export const Game1 = () => {
+    // Dealing with the Pokémon selection
+    const pokemonQueue = {};
+    const imageQueue = [];
+    const usedNumbers = [];
 
-    const [input, setInput] = useState("")
-    const [pokemon, setPokemon] = useState({} as any)
-
-    const rngesus = () => {
-        return Math.floor(Math.random() * (500 - 1 + 1) + 1)
-    }
-
-    const [randomId, setRandomId] = useState(rngesus())
-
-    useEffect(() => {
+    function fetchPokemon() {
         fetch(`https://pokeapi.co/api/v2/pokemon/${randomId}`)
             .then((response) => response.json())
-            .then((data) => setPokemon(data))
-    }, [randomId])
+            .then((data) => {
+                pokemonQueue[data.id] = ``
 
-    const [isWrong, setIsWrong] = useState(false)
+            });
+    }
 
-    const [pokemons, setPokemons] = useState<string[]>([])
+
+    const randomNumberGenerator = () => {
+        return Math.floor(Math.random() * (500 - 1 + 1) + 1);
+    };
+
+    // Definining states
+    const [input, setInput] = useState('');
+    const [pokemon, setPokemon] = useState({} as any);
+    const [randomId, setRandomId] = useState();
+    const [isWrong, setIsWrong] = useState(false);
+    const [pokemons, setPokemons] = useState<string[]>([]);
+
+    useEffect(() => {}, [randomId]);
 
     const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        let value = event.target.value
-        setInput(value)
-        value.split("").map((letter: string, index: number) => {
-            letter === pokemon.name.split("")[index] ? setIsWrong(false) : setIsWrong(true)
-        })
+        let value = event.target.value;
+        setInput(value);
+        value.split('').map((letter: string, index: number) => {
+            letter === pokemon.name.split('')[index]
+                ? setIsWrong(false)
+                : setIsWrong(true);
+        });
         if (value === pokemon.name) {
-            setPokemons([...pokemons, pokemon.name])
-            setRandomId(rngesus())
-            setInput("")
+            setPokemons([...pokemons, pokemon.name]);
+            setRandomId(randomNumberGenerator());
+            setInput('');
         }
-    }
+    };
 
-    console.log(pokemons)
+    console.log(pokemons);
 
-    const toProperCase = function(name: string) {
+    const toProperCase = function (name: string) {
         if (!!name) {
-            return name[0].toUpperCase() + name.substring(1).toLowerCase()
+            return name[0].toUpperCase() + name.substring(1).toLowerCase();
         }
-    }
-
+    };
 
     return (
         <Container>
             <Section>
                 <Title>Quem &eacute; esse Pok&eacute;mon?</Title>
-                <PokemonImage src={`https://unpkg.com/pokeapi-sprites@2.0.2/sprites/pokemon/other/dream-world/${randomId}.svg`} alt={pokemon.name} />
-                <TextInput id="pokemon-name" type="text" value={input} onChange={(e) => handleOnChange(e)} autoFocus className={isWrong ? 'shake' : ''} />
-                <Warning>Nota: Os nomes seguem a grafia em ingl&ecirc;s</Warning>
+                <PokemonImage
+                    alt={pokemon.name}
+                />
+                <TextInput
+                    id="pokemon-name"
+                    type="text"
+                    value={input}
+                    onChange={(e) => handleOnChange(e)}
+                    autoFocus
+                    className={isWrong ? 'shake' : ''}
+                />
+                <Warning>
+                    Nota: Os nomes seguem a grafia em ingl&ecirc;s
+                </Warning>
                 <p>Você acertou:</p>
                 <ul>
-                    {
-                        pokemons.map((p) => <li key={p}>{toProperCase(p)}</li>)
-                    }
+                    {pokemons.map((p) => (
+                        <li key={p}>{toProperCase(p)}</li>
+                    ))}
                 </ul>
             </Section>
         </Container>
     );
-}
+};
